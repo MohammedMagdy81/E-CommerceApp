fdf09267d820f33fcccd908c823bd46d34e35033
MohamedMagdy81 <mjdy7701@gmail.com>
2022-12-16 00:44:17 +0200

    design product detail fragment

---
+++ app/src/main/java/com/example/e_commerce/fragments/shopping/ProductDetailsFragment.kt
@@ -0,0 +1,12 @@
+package com.example.e_commerce.fragments.shopping
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.e_commerce.R
+
+class ProductDetailsFragment : Fragment() {
+
+}
